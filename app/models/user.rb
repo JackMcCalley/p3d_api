ecff5510a357bf815fec710c10e7c1298bdb77dd
jackmccalley <jackmccalley@gmail.com>
2018-09-28 12:19:21 -0700

    password format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :first_name, :last_name, :password_confirmation, presence: true
+    validates :password, presence: true, length: { minimum: 6 }
+    validates :email, presence: true, uniqueness: true
+
+    validates_format_of :password, with: /(?=.*[A-Z]+)(?=.*[0-9]+).*/
+
 end
