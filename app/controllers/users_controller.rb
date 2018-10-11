47f69b2761d6f418c1994af503e178b402c11fe0
jackmccalley <jackmccalley@gmail.com>
2018-10-10 17:21:37 -0700

    hi

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, except: [:show, :create]
-  # has_many :stlfiles, :dicomfiles
 
   def show
     user = User.find params[:id]
