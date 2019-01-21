e1cb380cfafb5c35fdd8d9be4257624b98f6a53a
John McCalley <jackmccalley@gmail.com>
2019-01-21 10:15:49 -0800

    implemented actionmailer hopefully

--- app/mailers/previews/case_submitted_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-class CaseSubmittedMailerPreview < ActionMailer::Preview
-  def case_submitted_mail_preview
-    @case = Rxform.where(case: params[:case])
-    CaseSubmittedMailer.case_submitted_email(case)
-  end
-end
