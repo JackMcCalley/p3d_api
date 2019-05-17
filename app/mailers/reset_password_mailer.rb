2d173f6972720497ca310867f5d119e0964088ad
John McCalley <jackmccalley@gmail.com>
2019-05-16 19:26:05 -0700

    changed apiurl for pass reset email

--- app/mailers/reset_password_mailer.rb
+++ app/mailers/reset_password_mailer.rb
@@ -1,7 +1,7 @@
 class ResetPasswordMailer < ApplicationMailer
   def reset_password_email(email, token)
     @user = User.find_by(email: email)
-    @url = "http://localhost:3001/reset-password/?params=#{token}"
+    @url = "https://www.p3dguides.com/reset-password/?params=#{token}"
     mail(
       to: email + '.com',
       subject: "P3D Guides Password Reset",
